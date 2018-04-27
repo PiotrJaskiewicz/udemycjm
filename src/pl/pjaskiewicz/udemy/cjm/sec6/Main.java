07fc44bf2f0cf5bf6cce14609de7fe87e80d4030
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-04-27 18:37:15 +0200

    Remove if else from exercise at section 6

--- src/pl/pjaskiewicz/udemy/cjm/sec6/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/Main.java
@@ -2,15 +2,7 @@ package pl.pjaskiewicz.udemy.cjm.sec6;
 
 public class Main {
     public static void main(String[] args) {
-        int value = 20;
-        if (value == 1) {
-            System.out.println("Value was 1");
-        } else if (value == 2) {
-            System.out.println("Value was 2");
-        } else {
-            System.out.println("Value was not 1 or 2");
-        }
-
+        
         int switchValue = 3;
         switch (switchValue) {
             case 1:
