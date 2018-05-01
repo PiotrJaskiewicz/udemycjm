ab389296753459dbb18257bc4d9cb63189e3ff2d
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-01 18:32:00 +0200

    Sec6 lec34, calculating interest by PJ

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -5,5 +5,12 @@ public class Main {
         return (amount * (interestRate / 100));
     }
 
-    
+    public static void main(String[] args) {
+        double amount = 1000;//PLN
+        double interestRate = 2; //%
+        System.out.println("Amount equal to: " + amount + "PLN");
+        System.out.println("Interest rate equal to: " + interestRate + "%");
+        System.out.println("Interest rate equal to: " + calculateInterest(amount, interestRate) + "PLN");
+    }
+
 }
