f81c983ddb049cf6f50710ff36e173a0c5afecde
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-01 18:28:58 +0200

    Sec6 lec34, add calculateInterest method

---
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -0,0 +1,9 @@
+package pl.pjaskiewicz.udemy.cjm.sec6.lec34;
+
+public class Main {
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+
+    
+}
