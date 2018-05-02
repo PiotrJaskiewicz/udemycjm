21899de952802890239b5f6c0c7d5b2d3d91aa86
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-02 15:09:38 +0200

    Sec6 lec34, challange interest loop

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("10000PLN at 2% interest = " + calculateInterest(10000, 2) + "PLN");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Loop " + i + " hello!");
+        for (int i = 2; i < 9; i++) {
+            System.out.println("Amount 10000PLN at " + i + "% interest rate equals " + calculateInterest(10000, i));
+        }
+        double interestRate = 1;
+
+        for (int i = 0; i < 7; i++) {
+            interestRate = interestRate + 1;
+            System.out.println("Amount 10000PLN at " + i + "% interest rate equals " + calculateInterest(10000, interestRate));
         }
     }
 
