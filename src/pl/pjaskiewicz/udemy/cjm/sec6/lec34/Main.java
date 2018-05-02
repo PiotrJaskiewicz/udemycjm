b4b2164e71828d9e29e7047e84a20b1ec1d0480d
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-02 16:23:09 +0200

    Sec6 lec34, challange interest loop by TB, add number round method

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -2,10 +2,10 @@ package pl.pjaskiewicz.udemy.cjm.sec6.lec34;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("10000PLN at 2% interest = " + calculateInterest(10000, 2) + "PLN");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Loop " + i + " hello!");
+
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10000PLN at " + i + "% interest = " + String.format("%.1f",calculateInterest(10000, i)) + "PLN");
         }
     }
 
