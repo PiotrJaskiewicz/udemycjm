a2b5c7787d5675855b0d49cac98719ebbfdf5d3c
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-02 14:53:32 +0200

    Sec6 lec34, for loop

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -1,9 +1,17 @@
 package pl.pjaskiewicz.udemy.cjm.sec6.lec34;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("10000PLN at 2% interest = " + calculateInterest(10000, 2) + "PLN");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+    }
+
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
     }
 
-    
+
 }
