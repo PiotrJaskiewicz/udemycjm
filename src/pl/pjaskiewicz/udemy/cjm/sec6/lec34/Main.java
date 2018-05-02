c18b3e349450d0438e65ee9b5bb516f3dc8c1738
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-02 19:42:50 +0200

    Sec6 lec34, add loop counter

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -3,11 +3,11 @@ package pl.pjaskiewicz.udemy.cjm.sec6.lec34;
 public class Main {
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 10; i < 50; i++){
+        for (int i = 2; i < 50; i++){
             if(isPrime(i)){
                 count++;
                 System.out.println("Number " + i + " is prime number");
-                if(count == 3){
+                if(count == 50){
 
                     System.out.println("Exiting for loop");
                     break;
@@ -34,12 +34,14 @@ public class Main {
         if (n == 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {//wystarczy sprawdzic do sqrt(n) bo na lewo i prawo od niego lezy tyle samo dzielnikow
+        for (int i = 2; i <= (long) Math.sqrt(n); i++) {//wystarczy sprawdzic do sqrt(n) bo na lewo i prawo od niego lezy tyle samo dzielnikow
+            System.out.println("Looping " + i);
             if (n % i == 0) {
                 return false;
             }
         }
 
         return true;
+
     }
 }
