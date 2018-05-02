39f4eb5555c0aaa479d0f61e67c7860ec48f762f
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-02 19:06:38 +0200

    Sec6 lec34, finding 3 prime numbers out of range TB

--- src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
+++ src/pl/pjaskiewicz/udemy/cjm/sec6/lec34/Main.java
@@ -2,22 +2,31 @@ package pl.pjaskiewicz.udemy.cjm.sec6.lec34;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        for (int i = 2; i < 9; i++) {
-            System.out.println("10000PLN at " + i + "% interest = " + String.format("%.1f", calculateInterest(10000, i)) + "PLN");
+        int count = 0;
+        for (int i = 10; i < 50; i++){
+            if(isPrime(i)){
+                count++;
+                System.out.println("Number " + i + " is prime number");
+                if(count == 3){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
         }
 
-        System.out.println("****************");
-        
-        for (int i = 8; i > 1; i--) {
-            System.out.println("10000PLN at " + i + "% interest = " + String.format("%.1f", calculateInterest(10000, i)) + "PLN");
+
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
         }
+        for (int i = 2; i <= Math.sqrt(n); i++) {//wystarczy sprawdzic do sqrt(n) bo na lewo i prawo od niego lezy tyle samo dzielnikow
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
-
-    public static double calculateInterest(double amount, double interestRate) {
-        return (amount * (interestRate / 100));
-    }
-
-
 }
