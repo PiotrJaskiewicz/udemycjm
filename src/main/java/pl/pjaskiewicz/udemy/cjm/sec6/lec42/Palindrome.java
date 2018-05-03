170a28fee201dc6919cf109ff8563f9f93469afd
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-03 18:08:09 +0200

    sec6lec42, Palindrom method

---
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec6/lec42/Palindrome.java
@@ -0,0 +1,25 @@
+package pl.pjaskiewicz.udemy.cjm.sec6.lec42;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-111111111));
+
+    }
+
+    public static boolean isPalindrome(int number) {
+        int reverse = 0;
+        int num = Math.abs(number);
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reverse = reverse * 10 + lastDigit;
+            num = num / 10;
+        }
+        if (reverse == Math.abs(number)) {
+            return true;
+
+        } else
+            return false;
+
+
+    }
+}
