182f7dd1952412223454a093220a74fd08879a1e
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-03 20:07:22 +0200

    sec6lec46, BankAccount class

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec6/lec46/Main.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec6/lec46/Main.java
@@ -1,4 +1,21 @@
 package pl.pjaskiewicz.udemy.cjm.sec6.lec46;
 
 public class Main {
+    public static void main(String[] args) {
+
+        BankAccount piotrAccount = new BankAccount();
+        piotrAccount.setBalance(1000);
+        System.out.println(piotrAccount.getBalance() + "PLN");
+
+        piotrAccount.depositFunds(550);
+        System.out.println(piotrAccount.getBalance() + "PLN");
+
+        piotrAccount.withdrawFunds(1500);
+        System.out.println(piotrAccount.getBalance() + "PLN");
+
+        piotrAccount.withdrawFunds(1500);
+        System.out.println(piotrAccount.getBalance() + "PLN");
+    }
+
+
 }
