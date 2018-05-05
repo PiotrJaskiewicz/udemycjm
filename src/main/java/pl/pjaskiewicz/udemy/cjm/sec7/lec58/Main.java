1ba12d2f4ea5355f9580e67aa5895f9ddaf0aeef
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-05 13:29:40 +0200

    sec6lec59, vehicle, car, outlander class

---
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec7/lec58/Main.java
@@ -0,0 +1,12 @@
+package pl.pjaskiewicz.udemy.cjm.sec7.lec58;
+
+public class Main {
+    public static void main(String[] args) {
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+        outlander.accelerate(-10);
+    }
+}
