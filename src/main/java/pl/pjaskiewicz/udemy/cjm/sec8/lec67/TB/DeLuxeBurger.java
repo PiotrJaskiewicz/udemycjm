471ffcb965d1d9eb1c895aeef88f47d3dee7ff17
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-23 21:13:09 +0200

    sec8lec67, burger challange

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec8/lec67/TB/DeLuxeBurger.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec8/lec67/TB/DeLuxeBurger.java
@@ -1,4 +1,29 @@
 package pl.pjaskiewicz.udemy.cjm.sec8.lec67.TB;
 
-public class DeLuxeBurger {
+public class DeLuxeBurger extends Hamburger {
+    public DeLuxeBurger() {
+        super("DeLuxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75); //dlaczego te metody mozna wywolac tylko w konstruktorze?
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a delux burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a delux burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a delux burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a delux burger");
+    }
 }
