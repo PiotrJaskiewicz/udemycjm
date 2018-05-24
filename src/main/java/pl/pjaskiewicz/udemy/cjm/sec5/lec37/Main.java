22f00cd8328ae0dda48bc629a99214542dfa6160
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-24 13:16:54 +0200

    sec5lec37, min, hours challange

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec5/lec37/Main.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec5/lec37/Main.java
@@ -2,7 +2,10 @@ package pl.pjaskiewicz.udemy.cjm.sec5.lec37;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getDurationString(150, 0));
+        System.out.println(getDurationString(123, 9));
+        System.out.println(getDurationString(7389));
+
+        System.out.println(leadingZeros(-1));
 
 
 
@@ -13,12 +16,22 @@ public class Main {
             int finalHours = minutes / 60;
             int finalMinutes = minutes % 60;
 
-            String hoursString = finalHours + "h";
+            return leadingZeros(finalHours) + "h " + leadingZeros(finalMinutes)+ "m " + leadingZeros(seconds)+ "s";
+
+            /*String hoursString = finalHours + "h"; // czemu h nie przechodzi dalej? tutaj hoursString mogloby chyba być cokolwiek bo i tak ponizej nadpisujemy?
             if (finalHours < 10) {
                 hoursString = "0" + finalHours;
             }
+            String minutesString = finalMinutes + "m";
+            if (finalMinutes < 10) {
+                minutesString = "0" + finalMinutes;
+            }
+            String secondsString = seconds + "s";
+            if (seconds < 10) {
+                secondsString = "0" + seconds;
+            }
 
-            return hoursString + "h " + finalMinutes + "m " + seconds + "s";
+            return hoursString + "h " + minutesString + "m " + secondsString + "s";    */
 
         }
         return "Invalid value";
@@ -33,5 +46,11 @@ public class Main {
 
         } else
             return "Invalid value";
+    }
+    public static String leadingZeros(int value){ //zamienia int na Stringa i dodaje zero jak cygra 0-9
+        if(value >=0 && value < 10){
+            return "0" + value;
+        }
+        return Integer.toString(value);
     }
 }
