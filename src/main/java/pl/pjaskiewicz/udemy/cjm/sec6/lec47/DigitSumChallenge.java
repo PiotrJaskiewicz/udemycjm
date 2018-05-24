84fb2625fc7464d25574acbc9392279718083e28
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-05-24 15:24:23 +0200

    sec6lec47, sum of all digits

---
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec6/lec47/DigitSumChallenge.java
@@ -0,0 +1,23 @@
+package pl.pjaskiewicz.udemy.cjm.sec6.lec47;
+
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+        int n = 123456789;
+        System.out.println("Sum of all digits for number " + n + " = " + sumDigits(n));
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        } else {
+            int sum = 0;
+            int temp = number;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp = temp / 10;
+            }
+            return sum;
+
+        }
+    }
+}
