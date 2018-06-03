8448005b0d3be10519c06f535fd1c717d9152b2f
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-06-03 10:31:01 +0200

    sec9lec75, grocery list

---
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec75/Main.java
@@ -0,0 +1,11 @@
+package pl.pjaskiewicz.udemy.cjm.sec9.lec75;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList(); //dlaczego tworzymy poza psvm?
+    public static void main(String[] args) {
+
+    }
+}
