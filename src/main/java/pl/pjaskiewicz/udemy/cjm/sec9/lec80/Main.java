baa9d2ed55ebdb17c5757d850896998350f133c5
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-07-11 15:51:13 +0200

    sec9lec80 minimum element challenge

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec80/Main.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec80/Main.java
@@ -1,4 +1,35 @@
 package pl.pjaskiewicz.udemy.cjm.sec9.lec80;
+//finding minimum value from given array
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(findMin(readIntegers(3)));
+
+    }
+
+    public static int[] readIntegers(int count) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[count];
+        for (int i = 0; i < count; i++) {
+            System.out.println("Type " + i + " element of array");
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("User array is: " + Arrays.toString(array));
+        return array;
+    }
+    public static int findMin(int[] array){
+        int min = array[0];
+        for(int i = 1; i<array.length;i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("Minimum value from users array is: ");
+        return min;
+    }
+
 }
