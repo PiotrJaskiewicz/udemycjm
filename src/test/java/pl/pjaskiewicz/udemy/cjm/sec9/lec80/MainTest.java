88d51a8258decee9b7ff342b3d5c94425f4b930f
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-07-12 10:36:39 +0200

    sec19lec293 unit testing with JUnit

---
+++ src/test/java/pl/pjaskiewicz/udemy/cjm/sec9/lec80/MainTest.java
@@ -0,0 +1,13 @@
+package pl.pjaskiewicz.udemy.cjm.sec9.lec80;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void findMinTest(){
+
+    }
+
+}
