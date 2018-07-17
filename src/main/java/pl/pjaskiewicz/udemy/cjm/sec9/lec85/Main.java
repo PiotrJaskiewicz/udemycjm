407e3bbdf73fb3db8d42b89fe21b928a99e1843e
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-07-17 11:54:47 +0200

    sec9lec85 Arraylist challange part1

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec85/Main.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec85/Main.java
@@ -1,4 +1,13 @@
 package pl.pjaskiewicz.udemy.cjm.sec9.lec85;
 
 public class Main {
+    public static void main(String[] args) {
+
+        MobilePhone mobilePhone = new MobilePhone("609479542");
+
+
+
+    }
+
+
 }
--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec85/MobilePhone.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec85/MobilePhone.java
@@ -1,4 +1,66 @@
 package pl.pjaskiewicz.udemy.cjm.sec9.lec85;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    // Create a program that implements a simple mobile phone with the following capabilities.
+    // Able to store, modify, remove and query contact names.
+    // You will want to create a separate class for Contacts (name and phone number).
+    // Create a master class (MobilePhone) that holds the ArrayList of Contacts
+    // The MobilePhone class has the functionality listed above.
+    // Add a menu of options that are available.
+    // Options:  Quit, print list of contacts, add new contact, update existing contact, remove contact
+    // and search/find contact.
+    // When adding or updating be sure to check if the contact already exists (use name)
+    // Be sure not to expose the inner workings of the Arraylist to MobilePhone
+    // e.g. no ints, no .get(i) etc
+    //
+    private String myNumber;
+    private ArrayList<Contact> myContacts = new ArrayList<>();
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>(); //Czy Conntact musi być to w nawiasie? new ArrayList<>(Contact);
+    }
+
+    public void printContacts() {
+        for (int i = 0; i < myContacts.size(); i++) {
+            System.out.println(myContacts.get(i));
+        }
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition<0){
+            System.out.println(oldContact.getName() + " was not found.");
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + " was replaced with " + newContact.getName());
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
