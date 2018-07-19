d91c18e6bacf258a541706f931ceab3809dcc456
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-07-19 13:28:13 +0200

    Autoboxing and unboxing - bank challange - PJ

--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec90/Bank.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec90/Bank.java
@@ -21,6 +21,7 @@ public class Bank {
     }
 
     private int findBranch(String branchName) {
+        //zwraca pozycje brancha. jak nie ma to -1
         for (int i = 0; i < listOfBranches.size(); i++) {
             Branch branch = listOfBranches.get(i);
             if (branch.getName().equals(branchName)) {
@@ -28,5 +29,28 @@ public class Bank {
             }
         }
         return -1;
+    }
+
+    private int findCustomerInBranch(String customerName, Branch branch) {
+        //szuka nazwy brancha i zwraca pozycję na liście
+        if(findBranch(branch.getName())>=0){
+            
+        }
+
+        for (int i = 0; i < listOfBranches.get(); i++) {
+            Customer customer = listOfCustomers.get(i);
+            if (customer.getName().equals(customerName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean addCustomerWithTransactionToBranch(Branch branch, Customer customer, Double transaction) {
+        if ()
+
+            branch.addCustomerWithTransaction(customer, transaction);
+        listOfBranches.add(branch);
+        return true;
     }
 }
--- src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec90/Main.java
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec9/lec90/Main.java
@@ -9,13 +9,14 @@ public class Main {
     // +The Customer class should have an arraylist of Doubles (transactions)
     // +Customer:
     // +Name, and the ArrayList of doubles.
-    // Branch:
+    // +Branch:
     // +Need to be able to add a new customer and initial transaction amount.
     // +Also needs to add additional transactions for that customer/branch
-    // Bank:
-    // Add a new branch
+    // +Bank:
+    // +Add a new branch
     // Add a customer to that branch with initial transaction
     // Add a transaction for an existing customer for that branch
+
     // Show a list of customers for a particular branch and optionally a list
     // of their transactions
     // Demonstration autoboxing and unboxing in your code
