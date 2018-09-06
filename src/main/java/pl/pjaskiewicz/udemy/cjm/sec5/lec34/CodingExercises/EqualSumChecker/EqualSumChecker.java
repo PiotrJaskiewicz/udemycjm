827d42479f9cad8ca489d6042a784cb0e81f3c51
PiotrJaskiewicz <pajaskiewicz@gmail.com>
2018-09-06 19:38:33 +0200

    equal sum checker

---
+++ src/main/java/pl/pjaskiewicz/udemy/cjm/sec5/lec34/CodingExercises/EqualSumChecker/EqualSumChecker.java
@@ -0,0 +1,11 @@
+package pl.pjaskiewicz.udemy.cjm.sec5.lec34.CodingExercises.EqualSumChecker;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int par1, int par2, int par3) {
+        if (par1 + par2 == par3) {
+            return true;
+
+        } else
+            return false;
+    }
+}
